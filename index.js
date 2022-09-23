07d96a85e5ebb8d938c8d620d6d8ae8ed65a9743
Kanjo Ngwu <111529504+kanjo0101@users.noreply.github.com>
2022-09-23 21:56:36 +0100

    Without Styling
    completed the functionality of the app without styling

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let firstPassWordEl = document.getElementById("first-password-el")
+let secondPassWordEl = document.getElementById("second-password-el")
+
+function firstPassword(){
+for( i = 0; i < 16; i++){
+  let indexNumber = Math.floor(Math.random() * characters.length)
+  // console.log(characters[indexNumber])
+  firstPassWordEl.textContent += characters[indexNumber]
+
+}
+}
+// firstPassword()
+
+function secondPassword(){
+  for( i = 0; i < 16; i++){
+    let indexNumber = Math.floor(Math.random() * characters.length)
+    // console.log(characters[indexNumber])
+    secondPassWordEl.textContent += characters[indexNumber]
+  
+  }
+
+}
+// secondPassword()
+function passWordGenerator(){
+  firstPassword()
+  secondPassword()
+}
+
+function deletePasswords(){
+  firstPassWordEl.textContent = ""
+  secondPassWordEl.textContent = ""
+}
 
 
 
